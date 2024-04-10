5040c8d53d92ee44fa82f2211a63a5051a28b748
Mark Poulsen <mjpoulsen@users.noreply.github.com>
2024-04-09 20:39:51 -0400

    add proxy for server socket.io

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
+    },
+    proxy: {
+      "/socket.io/": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
